92929ac2cdc3455cee8177ec9f988a4a6c4ae490
yerm <leeyelim98@gmail.com>
2024-07-16 10:32:28 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // const kakao = window.kakao;
+  // 일반 변수로 값을 할당시켜놓는것은 좋지 않은 방법임
+  const [kakao, setKakao] = useState(window.kakao);
+
+  // id="map"이 만들어진 후에(html이 만들어진 후) 코드가 동작되어야하기 때문에 useEffect 사용
+  useEffect(() => {
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3
+    };
+    var map = new kakao.maps.Map(container, options);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="map" style={
+        {
+          width: '500px', height: '400px'
+        }
+      }></div>
     </div>
   );
 }
